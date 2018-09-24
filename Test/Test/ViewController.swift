f4e41169136772fca7f704aa8e92166c844adef7
Guest User <Guest@administrators-macbook-pro-5.local>
2018-09-24 23:41:21 +0530

    First commit for bot

---
+++ Test/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Guest User on 24/09/18.
+//  Copyright © 2018 Rentomojo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
